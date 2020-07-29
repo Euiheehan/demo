9dea52e537beb5786de1998b35f72b69d442f895
euihe <euihee.han@fixelsoft.com>
2020-07-29 13:22:27 +0900

    start

---
+++ src/test/java/com/euihee/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.euihee.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
